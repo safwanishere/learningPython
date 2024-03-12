33a056599f549a53f11c2d2f3775c9a0819f6d5d
safwan <mohammedsafwan261205@gmail.com>
2024-03-12 23:32:18 +0530

    sets

---
+++ setClass.py
@@ -0,0 +1,20 @@
+set1 = {10, 56, 79, 90, "Madhu", 1.11}
+print(set1)
+set1 = {10, 56, 79, 90, "Madhu", 1.11, 10, 90}
+print(set1)
+set1 = {10, 56, 79, 90, "Madhu", 1.11, 10, 90, True, 1}
+print(set1)
+set1 = {10, 56, 79, 90, "Madhu", 1.11, 10, 90, True, 1}
+print(set1[3])
+set1 = {10, 56, 79, 90, "Madhu", 1.11, 10, 90,}
+set3 = {}
+set2 = set()
+print(type(set2))
+print(type(set3))
+
+set1[3] = 100
+set1.add(3)
+print(set1)
+print(len(set1))
+set1.remove(1)
+print(set1)
