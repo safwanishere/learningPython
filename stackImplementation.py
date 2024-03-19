7e73e6e30036ce835f9f2d4524bc52236970ee1a
safwan <mohammedsafwan261205@gmail.com>
2024-03-19 15:13:42 +0530

    stack implementation

---
+++ stackImplementation.py
@@ -0,0 +1,34 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            print("stack is empty")
+    
+    def peek(self):
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            print("stack is empty")
+
+    def size(self):
+        return len(self.items)
+    
+stack = Stack()
+stack.push(5)
+stack.push(7)
+stack.push(3)
+print(stack.peek())
+stack.pop()
+stack.push(1)
+stack.push(9)
+print(stack.peek())
