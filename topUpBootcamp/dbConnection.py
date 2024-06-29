3aa0927d3e02bcef8f098a468915705d4250568b
safwan <mohammedsafwan261205@gmail.com>
2024-06-29 10:22:48 +0530

    adding records sqlite3

--- topUpBootcamp/dbConnection.py
+++ topUpBootcamp/dbConnection.py
@@ -2,6 +2,10 @@ import sqlite3
 
 con = sqlite3.connect("database.db")
 cursor = con.cursor()
-cursor.execute("CREATE TABLE USERS(USERID INT(10), NAME VARCHAR(20), DOB DATE, PASSWORD VARCHAR(12));")
+
+# cursor.execute("CREATE TABLE USERS(USERID INT(10), NAME VARCHAR(20), DOB DATE, PASSWORD VARCHAR(12));")
+cursor.execute("DELETE FROM USERS;")
+cursor.execute("INSERT INTO USERS VALUES(1, 'Mohammed Safwan', '2005-12-26', '261205');") 
+
 con.commit()
 con.close()
